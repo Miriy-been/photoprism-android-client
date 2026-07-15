a7b995211f2dee8009b8b0902a827535a7a31a34
“ling” <2126925013@qq.com>
2026-07-15 22:12:50 +0800

    Task 5: Modify DeleteGalleryMediaUseCase to archive + record locally
    - Rewrite DeleteGalleryMediaUseCase to call archive() instead of delete()
    - Insert RecycleBinItem records with current timestamp on completion
    - Add PhotoPrismPhotosService and RecycleBinDao as constructor dependencies
    - Koin scopedOf(::DeleteGalleryMediaUseCase) auto-injects new deps

--- app/src/main/java/ua/com/radiokot/photoprism/features/gallery/logic/DeleteGalleryMediaUseCase.kt
+++ app/src/main/java/ua/com/radiokot/photoprism/features/gallery/logic/DeleteGalleryMediaUseCase.kt
@@ -1,32 +1,42 @@
 package ua.com.radiokot.photoprism.features.gallery.logic
 
 import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import ua.com.radiokot.photoprism.api.photos.service.PhotoPrismPhotosService
 import ua.com.radiokot.photoprism.features.gallery.data.storage.SimpleGalleryMediaRepository
+import ua.com.radiokot.photoprism.features.recyclebin.data.storage.RecycleBinDao
+import ua.com.radiokot.photoprism.features.recyclebin.data.storage.RecycleBinItem
 
-class DeleteGalleryMediaUseCase {
+class DeleteGalleryMediaUseCase(
+    private val photoPrismPhotosService: PhotoPrismPhotosService,
+    private val recycleBinDao: RecycleBinDao,
+) {
 
     /**
      * @param currentGalleryMediaRepository repository currently used to present the gallery,
-     * which will be updated on successful removal.
+     * which will be updated on successful archiving.
      */
     operator fun invoke(
         mediaUids: Collection<String>,
         currentGalleryMediaRepository: SimpleGalleryMediaRepository,
     ): Completable =
-        currentGalleryMediaRepository
-            .delete(
-                itemUids = mediaUids
+        currentGalleryMediaRepository.archive(itemUids = mediaUids)
+            .andThen(
+                Completable.defer {
+                    val now = System.currentTimeMillis()
+                    val items = currentGalleryMediaRepository.itemsList
+                    mediaUids.forEach { uid ->
+                        val media = items.find { it.uid == uid }
+                        recycleBinDao.insert(
+                            RecycleBinItem(
+                                photoUid = uid,
+                                archivedAt = now,
+                                thumbnailHash = media?.hash,
+                                photoTitle = media?.title,
+                            )
+                        )
+                    }
+                    Completable.complete()
+                }.subscribeOn(Schedulers.io())
             )
-
-    /**
-     * @param currentGalleryMediaRepository repository currently used to present the gallery,
-     * which will be updated on successful removal.
-     */
-    operator fun invoke(
-        mediaUid: String,
-        currentGalleryMediaRepository: SimpleGalleryMediaRepository,
-    ) = invoke(
-        setOf(mediaUid),
-        currentGalleryMediaRepository
-    )
 }
